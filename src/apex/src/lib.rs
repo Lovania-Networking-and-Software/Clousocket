e8166795ba174bde54070fa616add9baea6b7df7
Destovania <keremgoft@gmail.com>
2024-06-04 17:12:31 +0300

    Apex: unnecessary functions deleted.

--- src/apex/src/lib.rs
+++ src/apex/src/lib.rs
@@ -1,12 +1,6 @@
 use pyo3::prelude::*;
 use cached::{Cached, SizedCache};
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 #[pyclass]
 struct ACache {
     cache: SizedCache<String, String>
