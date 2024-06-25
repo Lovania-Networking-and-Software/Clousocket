c39d864f8b4fa61edb58db67642280986c63ce7e
Destovania <keremgoft@gmail.com>
2024-06-25 16:40:31 +0300

    Some linting.

--- src/handling.py
+++ src/handling.py
@@ -1,15 +1,35 @@
 #
 #  Copyright (C) 2024-present Lovania
 #
-import trio
 
-from src.middleware.serialisation import Command
+import typing
+
+from src.middleware.serialisation import Command, Data, End, SubCommand
 
 
 class Handler:
-    def __init__(self, consul, proto):
+    def __init__(self, consul):
         self.consul = consul
-        self.proto: trio.SocketStream = proto
+        self.ops: typing.Dict[str, typing.Awaitable] = dict()
 
-    def handle(self, data: Command):
-        pass
+    def add_command(self, name: str, op: typing.Awaitable):
+        self.ops[name] = op
+
+    async def handle(self, proto, data: Command):
+        raw_data = data
+        if isinstance(data.next, SubCommand):
+            op: typing.Awaitable = self.ops[f"{raw_data.this}_{raw_data.next.this}"]
+            raw_data = raw_data.next
+        else:
+            op: typing.Awaitable = self.ops[raw_data.this]
+        args = []
+        if raw_data.next == End:
+            pass
+        else:
+            while True:
+                if raw_data.next == End():
+                    break
+                elif isinstance(raw_data.next, Data):
+                    args.append(raw_data.next.this)
+                    raw_data = raw_data.next
+        await op(proto, *args)
