b5028a55e3b0e6ade53614473a8377fa91fc632b
Destovania <keremgoft@gmail.com>
2024-06-25 16:40:13 +0300

    Some changes for outlier detection

--- src/gatehouse/outliar_d.py
+++ src/gatehouse/outliar_d.py
@@ -1,25 +1,25 @@
+import numpy as np
+from sklearn.kernel_approximation import Nystroem
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import f1_score
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import SVC, OneClassSVM
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KernelDensity, KNeighborsTransformer
-from sklearn.kernel_approximation import Nystroem
-import numpy as np
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 
 class ReqClassifier:
     def __init__(self):
         self.x = []
+        self.y = []
         self.x_without_outlier = []
         self.test_x = []
         self.test_y = []
-        self.y = []
 
         self.prep_data()
         self.x = np.array(self.x)
         self.test_x = np.array(self.test_x)
+        self.x_without_outlier = np.array(self.x_without_outlier)
 
         self.kd = KernelDensity(kernel='epanechnikov', bandwidth="silverman", algorithm="ball_tree")
         self.ny = Nystroem(n_components=60)
@@ -31,19 +31,19 @@ class ReqClassifier:
         self.x = self.pp.fit_transform(self.x)
         self.kd.fit(self.x)
 
-        self.x = self.x*self.kd.score_samples(self.x).reshape(-1, 1)
+        self.x = self.x * self.kd.score_samples(self.x).reshape(-1, 1)
 
         self.test_x = self.pp.transform(self.test_x)
-        self.test_x = self.test_x*self.kd.score_samples(self.test_x).reshape(-1, 1)
+        self.test_x = self.test_x * self.kd.score_samples(self.test_x).reshape(-1, 1)
 
         self.cl = SGDClassifier(random_state=0)
 
         param_g = {
-            "loss": ['hinge', 'log_loss', 'modified_huber', 'squared_hinge', 'perceptron', 'squared_error', 'huber',
-                     'epsilon_insensitive', 'squared_epsilon_insensitive'],
+            "loss"   : ['hinge', 'log_loss', 'modified_huber', 'squared_hinge', 'perceptron', 'squared_error', 'huber',
+                        'epsilon_insensitive', 'squared_epsilon_insensitive'],
             "penalty": ['l2', 'l1', 'elasticnet'],
-            "tol": [0.01,  0.001, 0.0001, 0.00001],
-            "alpha": [0.01,  0.001, 0.0001, 0.00001],
+            "tol"    : [0.01, 0.001, 0.0001, 0.00001],
+            "alpha"  : [0.01, 0.001, 0.0001, 0.00001],
         }
         self.cl = GridSearchCV(self.cl, param_g, cv=5)
 
@@ -68,16 +68,16 @@ class ReqClassifier:
                 if command_cn == 3 and risk >= 7:
                     self.y.append(1)
                     self.test_y.append(1)
-                elif command_cn == 4 and risk >= 7:
+                elif command_cn == 4 and risk >= 6:
                     self.y.append(1)
                     self.test_y.append(1)
-                elif command_cn == 5 and risk >= 6:
+                elif command_cn == 5 and risk >= 5:
                     self.y.append(1)
                     self.test_y.append(1)
-                elif command_cn == 6 and risk >= 6:
+                elif command_cn == 6 and risk >= 4:
                     self.y.append(1)
                     self.test_y.append(1)
-                elif command_cn == 7 and risk >= 5:
+                elif command_cn == 7 and risk >= 3:
                     self.y.append(1)
                     self.test_y.append(1)
                 else:
@@ -87,17 +87,14 @@ class ReqClassifier:
         for command_cn in range(8, 11):
             for risk in range(0, 11):
                 self.test_x.append([risk, command_cn])
-                if risk < 3:
-                    self.test_y.append(0)
-                    continue
                 self.x.append([risk, command_cn])
-                if command_cn == 8 and risk >= 6:
+                if command_cn == 8 and risk >= 3:
                     self.y.append(1)
                     self.test_y.append(1)
-                elif command_cn == 9 and risk >= 6:
+                elif command_cn == 9 and risk >= 2:
                     self.y.append(1)
                     self.test_y.append(1)
-                elif command_cn == 10 and risk >= 5:
+                elif command_cn == 10 and risk >= 2:
                     self.y.append(1)
                     self.test_y.append(1)
                 else:
@@ -110,7 +107,7 @@ class ReqClassifier:
 
     def predict(self, risk, command_cn):
         x = self.pp.transform([[risk, command_cn]])
-        x = x*self.kd.score(x)
+        x = x * self.kd.score(x)
         return self.cl.predict(x)[0]
 
 
